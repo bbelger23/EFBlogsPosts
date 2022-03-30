83bd9caf1794c9c79530cb659dcbea50fa0ead4c
Brandon Belger <bbelger@wctc.edu>
2022-03-29 21:10:50 -0500

    added user input and displaying blogs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using NLog.Web;
 using System.IO;
+using System.Linq;
 namespace EFBlogsPosts
 {
     class Program
@@ -9,9 +10,31 @@ namespace EFBlogsPosts
         private static NLog.Logger logger = NLogBuilder.ConfigureNLog(Directory.GetCurrentDirectory() + "\\nlog.config").GetCurrentClassLogger();
         static void Main(string[] args)
         {
+            string selection = "";
             logger.Info("Program started");
 
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter your selection:");
+            Console.WriteLine("1. Display all blogs");
+            Console.WriteLine("2. Add blog");
+            Console.WriteLine("3. Create Post");
+            Console.WriteLine("4. Display Posts");
+            Console.WriteLine("Press q to quit");
+
+            selection = Console.ReadLine();
+
+            if (selection == "1") 
+            {
+                // Display all Blogs from the database
+                var db = new BloggingContext();
+                var query = db.Blogs.OrderBy(b => b.Name);
+
+                var total = db.Blogs.Count();
+                Console.WriteLine($"{total} blogs returned");
+                foreach (var item in query)
+                {
+                    Console.WriteLine(item.Name);
+                }
+            }
 
             logger.Info("Program ended");
         }
