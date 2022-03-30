0488160a130b5330db5f50603036647c7078d531
Brandon Belger <bbelger@wctc.edu>
2022-03-29 20:04:06 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EFBlogsPosts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
